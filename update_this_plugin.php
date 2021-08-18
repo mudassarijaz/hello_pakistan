06f0393aba826f9beda61ef26429e92fe8dfa062
Irfan Masood <irfanmasood@gmail.com>
2021-08-19 00:09:06 +0500

    Version 1.1 Ready

---
+++ update_this_plugin.php
@@ -0,0 +1,42 @@
+<?php 
+namespace NexVis\WordPress{
+	
+	require ( plugin_dir_path( __FILE__ ) ) . 'plugin_updater.php';
+
+class Update_This_Plugin extends WP_NVT_Plugin_Updater {
+
+	/**
+	 * Return the latest version number of the plugin.
+	 *
+	 * @return string
+	 */
+	protected function get_latest_version()
+	{
+        // Do anything you need to provide the latest version number.
+        // for example: perform a wp_remote_get() request to a Github repository retrieving the latest release or call out to your own server that hosts the plugin zip.
+		return '1.1.0';
+	}
+
+	/**
+	 * Get the plugin url.
+	 *
+	 * @return string The URL.
+	 */
+	protected function get_url()
+	{
+		return 'https://github.com/tarecord/my-plugin';
+	}
+
+	/**
+	 * Get the package url.
+	 *
+	 * @return string The package URL.
+	 */
+	protected function get_package_url()
+	{
+		return 'https://github.com/tarecord/my-plugin/archive/v1.0.0.zip';
+	}
+
+}
+}
+?>
